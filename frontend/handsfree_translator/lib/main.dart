827aab05d73cbe91d16aa2b02bb777664e5e1540
SungBeom-Choi <choisb3631@gmail.com>
2023-03-20 00:43:14 +0900

    Edit bluetooth module

--- frontend/handsfree_translator/lib/main.dart
+++ frontend/handsfree_translator/lib/main.dart
@@ -1,72 +1,167 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_blue/flutter_blue.dart';
-import 'package:permission_handler/permission_handler.dart';
+import 'package:flutter_blue_plus/flutter_blue_plus.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
-class _MyAppState extends State<MyApp> {
-  final FlutterBlue flutterBlue = FlutterBlue.instance;
-
-  Future<void> askForPermissions() async {
-    final status = await Permission.bluetooth.request();
-
-    if (status.isGranted) {
-      await Permission.location.request();
-    } else {
-      showDialog(
-        context: context,
-        builder: (BuildContext context) {
-          return AlertDialog(
-            title: const Text('Error'),
-            content: const Text('Bluetooth permission not granted'),
-            actions: <Widget>[
-              TextButton(
-                child: const Text('OK'),
-                onPressed: () {
-                  Navigator.of(context).pop();
-                },
-              ),
-            ],
-          );
-        },
-      );
-    }
-  }
-
-  void scanForDevices() async {
-    await askForPermissions();
-
-    await flutterBlue.startScan(timeout: const Duration(seconds: 4));
-
-    flutterBlue.scanResults.listen((List<ScanResult> results) {
-      for (ScanResult result in results) {
-        print('${result.device.name} found! rssi: ${result.rssi}');
-      }
-    });
-
-    flutterBlue.stopScan();
-  }
-
+  final title = 'Flutter BLE Scan Demo';
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Bluetooth Scan Example'),
+      title: title,
+      home: MyHomePage(title: title),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  FlutterBluePlus flutterBlue = FlutterBluePlus.instance;
+  List<ScanResult> scanResultList = [];
+
+  int scan_mode = 2; // scan mode
+  bool isScanning = false;
+
+  /* 생성자 */
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  /* 시작, 정지 */
+  void toggleState() {
+    isScanning = !isScanning;
+
+    if (isScanning) {
+      flutterBlue.startScan(
+          scanMode: ScanMode(scan_mode), allowDuplicates: true);
+      scan();
+    } else {
+      flutterBlue.stopScan();
+    }
+    setState(() {});
+  }
+
+  /* 
+  Scan Mode
+  Ts = scan interval 
+  Ds = duration of every scan window
+             | Ts [s] | Ds [s]
+  LowPower   | 5.120  | 1.024
+  BALANCED   | 4.096  | 1.024
+  LowLatency | 4.096  | 4.096
+
+  LowPower = ScanMode(0);
+  BALANCED = ScanMode(1);
+  LowLatency = ScanMode(2);
+
+  opportunistic = ScanMode(-1);
+   */
+
+  /* Scan */
+  void scan() async {
+    if (isScanning) {
+      // Listen to scan results
+
+      flutterBlue.scanResults.listen((results) {
+        // do something with scan results
+        scanResultList = results;
+        // update state
+        setState(() {});
+      });
+    }
+  }
+
+  /* 장치의 RSSI */
+  Widget deviceSignal(ScanResult r) {
+    return Text(r.rssi.toString());
+  }
+
+  /* 장치의 MAC 주소 위젯  */
+  Widget deviceMacAddress(ScanResult r) {
+    return Text(r.device.id.id);
+  }
+
+  /* 장치의 명 위젯  */
+  Widget deviceName(ScanResult r) {
+    String name;
+
+    if (r.device.name.isNotEmpty) {
+      // device.name에 값이 있다면
+      name = r.device.name;
+    } else if (r.advertisementData.localName.isNotEmpty) {
+      // advertisementData.localName에 값이 있다면
+      name = r.advertisementData.localName;
+    } else {
+      // 둘다 없다면 이름 알 수 없음...
+      name = 'N/A';
+    }
+    return Text(name);
+  }
+
+  /* BLE 아이콘 위젯 */
+  Widget leading(ScanResult r) {
+    return const CircleAvatar(
+      backgroundColor: Colors.cyan,
+      child: Icon(
+        Icons.bluetooth,
+        color: Colors.white,
+      ),
+    );
+  }
+
+  /* 장치 아이템을 탭 했을때 호출 되는 함수 */
+  void onTap(ScanResult r) {
+    // 단순히 이름만 출력
+    print(r.device.name);
+  }
+
+  /* 장치 아이템 위젯 */
+  Widget listItem(ScanResult r) {
+    return ListTile(
+      onTap: () => onTap(r),
+      leading: leading(r),
+      title: deviceName(r),
+      subtitle: deviceMacAddress(r),
+      trailing: deviceSignal(r),
+    );
+  }
+
+  /* UI */
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        /* 장치 리스트 출력 */
+        child: ListView.separated(
+          itemCount: scanResultList.length,
+          itemBuilder: (context, index) {
+            return listItem(scanResultList[index]);
+          },
+          separatorBuilder: (BuildContext context, int index) {
+            return const Divider();
+          },
         ),
-        body: Center(
-          child: ElevatedButton(
-            onPressed: scanForDevices,
-            child: const Text('Scan for Devices'),
-          ),
-        ),
+      ),
+      /* 장치 검색 or 검색 중지  */
+      floatingActionButton: FloatingActionButton(
+        onPressed: toggleState,
+        // 스캔 중이라면 stop 아이콘을, 정지상태라면 search 아이콘으로 표시
+        child: Icon(isScanning ? Icons.stop : Icons.search),
       ),
     );
   }
--- frontend/handsfree_translator/macos/Flutter/GeneratedPluginRegistrant.swift
+++ frontend/handsfree_translator/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,10 +5,8 @@
 import FlutterMacOS
 import Foundation
 
-import flutter_blue
 import path_provider_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  FlutterBluePlugin.register(with: registry.registrar(forPlugin: "FlutterBluePlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
 }
